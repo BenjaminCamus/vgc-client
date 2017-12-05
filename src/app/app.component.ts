ecf6add6e47f2b7006cde3886f046bc2be3f0de5
Ben <benjamin@camus.me>
2017-12-05 19:07:57 +0100

    title VGC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-    title = 'COLLECTORAMA';
+    title = 'VGC';
 
     get currentUser(): any {
         return JSON.parse(localStorage.getItem('currentUser'));

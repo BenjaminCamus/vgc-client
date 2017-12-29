13278100e57bc238cd93de8c7609b4c70fec890c
Ben <benjamin@camus.me>
2017-12-29 17:37:32 +0100

    ngif display filters

--- src/app/_animations/router.animations.ts
+++ src/app/_animations/router.animations.ts
@@ -5,66 +5,28 @@ export function routerTransition() {
 }
 function slideToBottom() {
   return trigger('routerTransition', [
-    // state('void', style({position:'absolute', width: '100%'}) ),
-    // state('*', style({position:'absolute', width: '100%'}) ),
-    // transition(':enter', [
-    //   style({opacity: '0', transform: 'scale(0.9)', zIndex: '10'}),
-    //   animate('0.5s 0.5s ease-in-out', style({opacity: '1', transform: 'scale(1)'}))
-    // ]),
-    // transition(':leave', [
-    //   //style({transform: 'translateY(0%)'}),
-    //   style({zIndex: '1'}),
-    //   animate('0.5s ease-in-out', style({opacity: '0', transform: 'scale(1.1)'}))
-    // ])
-
-
-
     state('none', style({position:'absolute'}) ),
     state('void', style({position:'absolute'}) ),
     state('*', style({position:'absolute'}) ),
-    // transition('void => none', [
-    //   style({transform: 'translateY(-100%)', zIndex: '10'}),
-    //   animate('0.5s ease-in-out', style({transform: 'translateY(0)'}))
-    // ]),
-    // transition('none => *', [
-    //   style({transform: 'translateY(0)', zIndex: '10'}),
-    //   animate('0.5s ease-in-out', style({transform: 'translateY(100%)'}))
-    // ]),
-    // transition('void => none', [
-    //   style({transform: 'translateY(-100%) scale(0.8)', zIndex: '10'}),
-    //   animate('0.2s 0.5s ease-in-out', style({transform: 'translateY(0) scale(0.8)'}))
-    // ]),
-    // transition('none => *', [
-    //   style({transform: 'translateY(0)', zIndex: '10'}),
-    //   animate('0.5s ease-in-out', style({transform: 'translateY(100%)'}))
-    // ]),
+
     // transition('void => *', [
-    //   style({position:'absolute', transform: 'translateY(-100%)'}),
-    //   animate('0.5s ease-in-out', style({transform: 'translateY(0)'})),
+    //   style({opacity: '0'}),
+    //   animate('0.5s 0.5s ease-in-out', style({opacity: '1'})),
     // ]),
-    // transition('* => void', [
-    //   style({position:'absolute', transform: 'translateY(0)'}),
-    //   animate('0.5s ease-in-out', style({transform: 'translateY(100%)'})),
-    // ])
-    transition('void => *', [
-      style({opacity: '0'}),
-      animate('0.5s 0.5s ease-in-out', style({opacity: '1'})),
-    ]),
     transition('* => void', [
       style({opacity: '1'}),
       animate('0.5s ease-in-out', style({opacity: '0'})),
+    ]),
+
+    transition('void => *', [
+      style({position:'absolute', transform: 'translateY(-100%)', zIndex: 999}),
+      animate('0.5s ease-in-out', style({transform: 'scale(0.95)'})),
+      animate('0.5s ease-in-out', style({transform: 'translateY(0)'})),
+      animate('0.5s ease-in-out', style({transform: 'scale(1)'})),
     ])
-    // transition('void => *', [
-    //   style({position:'absolute', transform: 'translateY(-100%)'}),
-    //   animate('0.5s ease-in-out', style({transform: 'scale(0.8)'})),
-    //   animate('0.5s ease-in-out', style({transform: 'translateY(0)'})),
-    //   animate('0.5s ease-in-out', style({transform: 'scale(1)'})),
-    // ]),
     // transition('* => void', [
-    //   style({position:'absolute', transform: 'translateY(0)'}),
-    //   animate('0.5s ease-in-out', style({transform: 'scale(0.8)'})),
-    //   animate('0.5s ease-in-out', style({transform: 'translateY(100%)'})),
-    //   animate('0.5s ease-in-out', style({transform: 'scale(1)'})),
-    // ])
+    //   style({position:'absolute', transform: 'translateY(0)', zIndex:0}),
+    //   animate('2s ease-in-out', style({transform: 'scale(0)'})),
+    // ]),
   ]);
 }

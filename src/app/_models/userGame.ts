2294900a08660cd1f2fe2074a7173a7526849043
Ben <benjamin@camus.me>
2018-01-18 21:55:50 +0100

    userGame fields

--- src/app/_models/userGame.ts
+++ src/app/_models/userGame.ts
@@ -29,4 +29,30 @@ export class UserGame {
     saleContact: Contact;
 
     note: string;
+
+    fields = [];
+
+    constructor() {
+
+        this.fields['progress'] = {type: 'string', label: 'Progression'};
+        this.fields['platform.name'] = {type: 'string', label: 'Plateforme'};
+        this.fields['game.name'] = {type: 'string', label: 'Titre'};
+        this.fields['version'] = {type: 'string', label: 'Verion'};
+        this.fields['state'] = {type: 'state', label: 'Etat'};
+        this.fields['rating'] = {type: 'string', label: 'Note'};
+        this.fields['pricePaid'] = {type: 'price', label: 'Payé'};
+        this.fields['priceAsked'] = {type: 'price', label: 'Demandé'};
+        this.fields['purchaseDate'] = {type: 'date', label: 'Date Achat'};
+        this.fields['purchasePlace'] = {type: 'name', label: 'Lieu Achat'};
+        this.fields['purchaseContact'] = {type: 'name', label: 'Vendeur'};
+        this.fields['priceResale'] = {type: 'price', label: 'Estimation'};
+        this.fields['priceSold'] = {type: 'price', label: 'Vendu', hiddenMD: true};
+        this.fields['saleDate'] = {type: 'date', label: 'Date Vente', hiddenMD: true};
+        this.fields['salePlace'] = {type: 'name', label: 'Lieu Vente', hiddenMD: true};
+        this.fields['saleContact'] = {type: 'name', label: 'Acheteur', hiddenMD: true};
+
+        for (let field in this.fields) {
+            this.fields[field].name = field;
+        }
+    }
 }

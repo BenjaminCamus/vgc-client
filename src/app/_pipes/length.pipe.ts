1ce2cd7841f9e920b5150cb9b0c77fb104980e92
Ben <benjamin@camus.me>
2018-01-27 23:43:32 +0100

    length pipe + total in title

---
+++ src/app/_pipes/length.pipe.ts
@@ -0,0 +1,13 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+    name: 'length',
+    pure: false
+})
+
+export class LengthPipe implements PipeTransform {
+
+    transform(value: any): number {
+        return value.length;
+    }
+}

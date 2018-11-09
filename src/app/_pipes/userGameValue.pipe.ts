7b52477a9c1dd5919e087f35481569099fb9339c
Ben <benjamin@camus.me>
2018-11-09 23:00:38 +0100

    nl2br note

--- src/app/_pipes/userGameValue.pipe.ts
+++ src/app/_pipes/userGameValue.pipe.ts
@@ -120,6 +120,14 @@ export class UserGameValuePipe {
                 break;
         }
 
-        return value;
+        return nl2br(value, true);
     }
+}
+
+function nl2br (str, is_xhtml) {
+    if (typeof str === 'undefined' || str === null) {
+        return '';
+    }
+    var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+    return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
 }
